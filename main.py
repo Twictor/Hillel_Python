299420ab0859fb4d29dea958f83a89e57b0d6716
twictor <twictor28@outlook.com>
2025-01-29 11:44:36 +0200

    123

--- main.py
+++ main.py
@@ -1,9 +1,27 @@
-print('Hello World!')
+# print('Hello World!')
+# print('Hello')
+#
+# numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+# numbers.reverse()
+# print(numbers)
+#
+# print('Hello git!')
+# # Проверка на РЕР8
+#
+# greeting = 'Hello, world!'
+# for char in greeting:
+#     print(char)
+
+# user = int(input("Enter a 5-digit number: "))
+# num0 = user // 10000
+# user1 = user - (num0 * 10000)
+# num1 = user1 // 1000
+# user2 = user1 - (num1 * 1000)
+# num2 = user2 // 100
+# user3 = user2 - (num2 * 100)
+# num3 = user3 // 10
+# num4 = user3 - (num3 * 10)
+# print(num4 * 10000 + num3 * 1000 + num2 * 100 + num1 * 10 + num0)
+
 print('Hello')
 
-numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
-numbers.reverse()
-print(numbers)
-
-print('Hello git!')
-# Проверка на РЕР8
